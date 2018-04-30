07695babe49f8aeb58e69efa0bbcb0b7e4f7e801
Dwiky Erlangga <dwikyerl@gmail.com>
2018-04-30 23:35:08 +0700

    Finish Number in Words Challenge

--- number_in_words.js
+++ number_in_words.js
@@ -1,5 +1,27 @@
 function numberToWords(number) {
-  // Your code here
+  const numbers = ["", 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+
+  if (number >= 1000000000000) {
+    return `${numberToWords(Math.floor(number/1000000000000))} triliun ${numberToWords(number%1000000000000)}`;
+  } else if (number >= 1000000000) {
+    return `${numberToWords(Math.floor(number/1000000000))} milyar ${numberToWords(number%1000000000)}`;
+  } else if (number >= 1000000) {
+    return `${numberToWords(Math.floor(number/1000000))}${number % 10000000 === 0 ? '' : ' '}juta ${numberToWords(number%1000000)}`;
+  } else if (number >= 2000) {
+    return `${numberToWords(Math.floor(number/1000))}${number % 10000 === 0 ? '' : ' '}ribu ${numberToWords(number%1000)}`;
+  } else if (number >= 1000) {
+    return `seribu ${numberToWords(number-1000)}`;
+  } else if (number >= 200){
+    return `${numberToWords(Math.floor(number/100))} ratus ${numberToWords(number%100)}`;
+  } else if (number >= 100) {
+    return `seratus ${numberToWords(number-100)}`;
+  } else if (number >= 20) {
+    return `${numberToWords(Math.floor(number/10))} puluh ${numberToWords(number%10)}`;
+  } else if (number >= 12) {
+    return `${numberToWords(number-10)} belas`;
+  } else if (number >= 0) {
+    return numbers[number];
+  }
 }
 
 // Driver code
