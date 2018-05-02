ccd0bf025bbd4f5ed1e9830af816f117afd8a7f1
Tushar <tusharbedi7373@hotmail.com>
2018-05-02 11:34:20 +0700

    updated codes for some cases

---
+++ number_in_words.js
@@ -0,0 +1,40 @@
+function numberToWords(number) {
+
+  const words = ['','satu','dua','tiga','empat','lima','enam','tuju','delapan','sembilan','sepuluh','sebelas' ];
+
+  let output = '';
+
+  if (number < 0) {
+    return 'Invalid Number'
+  }
+  else if (number < 12) {
+    output += words[number]
+  }
+  else if (number < 20) {
+    output += (words[number - 10] + ' belas')
+  }
+  else if (number < 100) {
+    output += words[Math.floor(number/10)] + ' puluh ' + words[number%10]
+  }
+  else if (number < 200) {
+    output += 'seratus ' + numberToWords(number - 100)
+  }
+  else if (number < 1000) {
+    output += words[Math.floor(number/100)] + ' ratus ' + numberToWords(number % 100);
+  }
+  else if (number < 1000000) {
+    output += numberToWords(Math.floor(number / 1000)) + ' ribu ' + numberToWords(number % 1000);
+
+  }
+  else if (number < 1000000000) {
+    output += numberToWords(Math.floor(number / 1000000)) + ' juta ' + numberToWords(number % 1000000);
+
+  }
+  else if (number < 1000000000000) {
+    output += numberToWords(Math.floor(number / 1000000000)) + 'milyar ' + numberToWords(number % 1000000000);
+  }
+
+  return output;
+}
+
+console.log(numberToWords(25536))
